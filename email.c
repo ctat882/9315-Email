b03bf297f4c1270600f701c5984a328d2c3700f9
Corey Tattam <corey_tattam@hotmail.com>
2014-08-23 02:00:40 +1000

    Email_in

--- email.c
+++ email.c
@@ -9,72 +9,210 @@
 
 #include "postgres.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "fmgr.h"
 #include "libpq/pqformat.h"		/* needed for send/recv functions */
 
+#define MAX_CHARS  128
+#define TRUE 1
+#define FALSE 0
 
 PG_MODULE_MAGIC;
 
-typedef struct Complex
+typedef struct EmailAddress
 {
-	double		x;
-	double		y;
-}	Complex;
+	char		*local;
+	char		*domain;
+}	EmailAddress;
 
 /*
  * Since we use V1 function calling convention, all these functions have
  * the same signature as far as C is concerned.  We provide these prototypes
  * just to forestall warnings when compiled with gcc -Wmissing-prototypes.
  */
-Datum		complex_in(PG_FUNCTION_ARGS);
-Datum		complex_out(PG_FUNCTION_ARGS);
-Datum		complex_recv(PG_FUNCTION_ARGS);
-Datum		complex_send(PG_FUNCTION_ARGS);
-Datum		complex_add(PG_FUNCTION_ARGS);
-Datum		complex_abs_lt(PG_FUNCTION_ARGS);
-Datum		complex_abs_le(PG_FUNCTION_ARGS);
-Datum		complex_abs_eq(PG_FUNCTION_ARGS);
-Datum		complex_abs_ge(PG_FUNCTION_ARGS);
-Datum		complex_abs_gt(PG_FUNCTION_ARGS);
-Datum		complex_abs_cmp(PG_FUNCTION_ARGS);
+
+/* Functions concerning the reading and displaying of type EmailAddress */ 
+Datum		email_in(PG_FUNCTION_ARGS);
+Datum		email_out(PG_FUNCTION_ARGS);
+/*Datum		email_recv(PG_FUNCTION_ARGS);*/
+/*Datum		email_send(PG_FUNCTION_ARGS);*/
+
+/* Functions concerning the reading and displaying of EmailAddress */ 
+/*Datum		email_add(PG_FUNCTION_ARGS);*/
+/*Datum		email_abs_lt(PG_FUNCTION_ARGS);*/
+/*Datum		email_abs_le(PG_FUNCTION_ARGS);*/
+/*Datum		email_abs_eq(PG_FUNCTION_ARGS);*/
+/*Datum		email_abs_ge(PG_FUNCTION_ARGS);*/
+/*Datum		email_abs_gt(PG_FUNCTION_ARGS);*/
+/*Datum		email_abs_cmp(PG_FUNCTION_ARGS);*/
 
 
 /*****************************************************************************
  * Input/Output functions
  *****************************************************************************/
 
-PG_FUNCTION_INFO_V1(complex_in);
+PG_FUNCTION_INFO_V1(email_in);
+
+/*Datum*/
+/*email_in(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	char	   *str = PG_GETARG_CSTRING(0);*/
+/*	double		x,*/
+/*				y;*/
+/*	Complex    *result;*/
+
+/*	if (sscanf(str, " ( %lf , %lf )", &x, &y) != 2)*/
+/*		ereport(ERROR,*/
+/*				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),*/
+/*				 errmsg("invalid input syntax for complex: \"%s\"",*/
+/*						str)));*/
+
+/*	result = (Complex *) palloc(sizeof(Complex));*/
+/*	result->x = x;*/
+/*	result->y = y;*/
+/*	PG_RETURN_POINTER(result);*/
+/*}*/
+
+int isValidCharacter (char c);
+int getLocalStringEnd (char *str, int len);
+int getDomainStringEnd (int start, char *str, int len);
 
 Datum
-complex_in(PG_FUNCTION_ARGS)
+email_in(PG_FUNCTION_ARGS)
 {
-	char	   *str = PG_GETARG_CSTRING(0);
-	double		x,
-				y;
-	Complex    *result;
+	char *str = PG_GETARG_CSTRING(0);
+	EmailAddress *result;
 
-	if (sscanf(str, " ( %lf , %lf )", &x, &y) != 2)
+	/* Scan string and split into substrings local and domain*/
+	int str_len = strlen(str);
+	int i = 0;
+	i = getLocalStringEnd(str, str_len);
+	if (!i) {
 		ereport(ERROR,
 				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-				 errmsg("invalid input syntax for complex: \"%s\"",
+				 errmsg("invalid input syntax for email address: \"%s\"",
 						str)));
+	}
+	char local[i];
+	memcpy(local, &str[0],i - 1);
+	local[i - 1] = '\0';
 
-	result = (Complex *) palloc(sizeof(Complex));
-	result->x = x;
-	result->y = y;
+	int at_pos = i;
+	i = getDomainStringEnd (i,str,str_len);
+	if (!i) {
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+				 errmsg("invalid input syntax for email address: \"%s\"",
+						str)));
+	}
+	char domain[i - (at_pos + 1)];
+	memcpy(domain, &str[at_pos],i - 1);
+	local[i - 1] = '\0';
+
+	/* Allocate memory */ 
+	result = (EmailAddress *) palloc(sizeof(EmailAddress));
+	result->local = local;
+	result->domain = domain;
 	PG_RETURN_POINTER(result);
 }
 
-PG_FUNCTION_INFO_V1(complex_out);
+/**
+	Does basic character validation for the domain substring.
+	If an error is detected, 0 is returned.
+*/
+int getDomainStringEnd (int start, char *str, int len) {
+	int i = start;
+	int end_pos = 0;
+	int error = FALSE;
+	char c = 0;
+	for (i = i; i < len; i += 1) {
+		c = str[i];
+		printf("%c",c);
+		if (!isValidCharacter(c)) {
+			printf("Invalid Character\n");
+			error = TRUE;
+			break;
+		}
+		else if (c == '@') {
+			printf("Invalid Character\n");
+			error = TRUE;
+			break;
+		}		
+	}
+	if (!error) {
+		end_pos = i;
+	}
+	return end_pos;
+}
+
+
+/**
+	Given a string, this function iterates over it until
+	the first '@' symbol is found, return it's position.
+	If an error is detected, 0 will be returned.
+*/
+
+int getLocalStringEnd (char *str, int len) {
+	int i = 0;
+	int end_pos = 0;
+	int error = FALSE;
+	char c = 0;
+	for (i = 0; i < len && c != '@'; i++)
+	{
+		c = str[i];
+		printf("%c",c);
+		if (!isValidCharacter(c)) {
+			printf("Invalid Character\n");
+			error = TRUE;
+			break;
+		}
+		else if (i == 0 && c == '@') {
+			printf("No Local\n");
+			error = TRUE;
+			break;
+		}			  	
+	}
+	if (c == '@' && i >= len) {
+		printf("No Domain\n");
+		error = TRUE;
+	}
+	if (!error) {
+		end_pos = i;
+	}
+	return end_pos;
+}
+
+int isValidCharacter (char c) {
+	int valid = TRUE;
+	if ( c < 48 && c != 46 && c != 45) {
+		valid = FALSE;
+	}
+	else if (c > 57 && c < 64) {
+		valid = FALSE;
+	}
+	else if (c > 90 && c < 97) {
+		valid = FALSE;
+	}
+	else if (c > 122) {
+		valid = FALSE;
+	}
+	return valid;
+}
+
+
+PG_FUNCTION_INFO_V1(email_out);
 
 Datum
-complex_out(PG_FUNCTION_ARGS)
+email_out(PG_FUNCTION_ARGS)
 {
-	Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
+	EmailAddress    *email = (EmailAddress *) PG_GETARG_POINTER(0);
 	char	   *result;
 
-	result = (char *) palloc(100);
-	snprintf(result, 100, "(%g,%g)", complex->x, complex->y);
+	result = (char *) palloc(256);
+	snprintf(result, 256, "%s@%s", email->local, email->domain);
 	PG_RETURN_CSTRING(result);
 }
 
@@ -84,33 +222,33 @@ complex_out(PG_FUNCTION_ARGS)
  * These are optional.
  *****************************************************************************/
 
-PG_FUNCTION_INFO_V1(complex_recv);
+/*PG_FUNCTION_INFO_V1(complex_recv);*/
 
-Datum
-complex_recv(PG_FUNCTION_ARGS)
-{
-	StringInfo	buf = (StringInfo) PG_GETARG_POINTER(0);
-	Complex    *result;
+/*Datum*/
+/*complex_recv(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	StringInfo	buf = (StringInfo) PG_GETARG_POINTER(0);*/
+/*	Complex    *result;*/
 
-	result = (Complex *) palloc(sizeof(Complex));
-	result->x = pq_getmsgfloat8(buf);
-	result->y = pq_getmsgfloat8(buf);
-	PG_RETURN_POINTER(result);
-}
+/*	result = (Complex *) palloc(sizeof(Complex));*/
+/*	result->x = pq_getmsgfloat8(buf);*/
+/*	result->y = pq_getmsgfloat8(buf);*/
+/*	PG_RETURN_POINTER(result);*/
+/*}*/
 
-PG_FUNCTION_INFO_V1(complex_send);
+/*PG_FUNCTION_INFO_V1(complex_send);*/
 
-Datum
-complex_send(PG_FUNCTION_ARGS)
-{
-	Complex    *complex = (Complex *) PG_GETARG_POINTER(0);
-	StringInfoData buf;
+/*Datum*/
+/*complex_send(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	Complex    *complex = (Complex *) PG_GETARG_POINTER(0);*/
+/*	StringInfoData buf;*/
 
-	pq_begintypsend(&buf);
-	pq_sendfloat8(&buf, complex->x);
-	pq_sendfloat8(&buf, complex->y);
-	PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
-}
+/*	pq_begintypsend(&buf);*/
+/*	pq_sendfloat8(&buf, complex->x);*/
+/*	pq_sendfloat8(&buf, complex->y);*/
+/*	PG_RETURN_BYTEA_P(pq_endtypsend(&buf));*/
+/*}*/
 
 /*****************************************************************************
  * New Operators
@@ -118,20 +256,20 @@ complex_send(PG_FUNCTION_ARGS)
  * A practical Complex datatype would provide much more than this, of course.
  *****************************************************************************/
 
-PG_FUNCTION_INFO_V1(complex_add);
+/*PG_FUNCTION_INFO_V1(complex_add);*/
 
-Datum
-complex_add(PG_FUNCTION_ARGS)
-{
-	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
-	Complex    *result;
+/*Datum*/
+/*complex_add(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	Complex    *a = (Complex *) PG_GETARG_POINTER(0);*/
+/*	Complex    *b = (Complex *) PG_GETARG_POINTER(1);*/
+/*	Complex    *result;*/
 
-	result = (Complex *) palloc(sizeof(Complex));
-	result->x = a->x + b->x;
-	result->y = a->y + b->y;
-	PG_RETURN_POINTER(result);
-}
+/*	result = (Complex *) palloc(sizeof(Complex));*/
+/*	result->x = a->x + b->x;*/
+/*	result->y = a->y + b->y;*/
+/*	PG_RETURN_POINTER(result);*/
+/*}*/
 
 
 /*****************************************************************************
@@ -145,84 +283,84 @@ complex_add(PG_FUNCTION_ARGS)
  * an internal three-way-comparison function, as we do here.
  *****************************************************************************/
 
-#define Mag(c)	((c)->x*(c)->x + (c)->y*(c)->y)
+/*#define Mag(c)	((c)->x*(c)->x + (c)->y*(c)->y)*/
 
-static int
-complex_abs_cmp_internal(Complex * a, Complex * b)
-{
-	double		amag = Mag(a),
-				bmag = Mag(b);
+/*static int*/
+/*complex_abs_cmp_internal(Complex * a, Complex * b)*/
+/*{*/
+/*	double		amag = Mag(a),*/
+/*				bmag = Mag(b);*/
 
-	if (amag < bmag)
-		return -1;
-	if (amag > bmag)
-		return 1;
-	return 0;
-}
+/*	if (amag < bmag)*/
+/*		return -1;*/
+/*	if (amag > bmag)*/
+/*		return 1;*/
+/*	return 0;*/
+/*}*/
 
 
-PG_FUNCTION_INFO_V1(complex_abs_lt);
+/*PG_FUNCTION_INFO_V1(complex_abs_lt);*/
 
-Datum
-complex_abs_lt(PG_FUNCTION_ARGS)
-{
-	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+/*Datum*/
+/*complex_abs_lt(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	Complex    *a = (Complex *) PG_GETARG_POINTER(0);*/
+/*	Complex    *b = (Complex *) PG_GETARG_POINTER(1);*/
 
-	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) < 0);
-}
+/*	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) < 0);*/
+/*}*/
 
-PG_FUNCTION_INFO_V1(complex_abs_le);
+/*PG_FUNCTION_INFO_V1(complex_abs_le);*/
 
-Datum
-complex_abs_le(PG_FUNCTION_ARGS)
-{
-	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+/*Datum*/
+/*complex_abs_le(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	Complex    *a = (Complex *) PG_GETARG_POINTER(0);*/
+/*	Complex    *b = (Complex *) PG_GETARG_POINTER(1);*/
 
-	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) <= 0);
-}
+/*	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) <= 0);*/
+/*}*/
 
-PG_FUNCTION_INFO_V1(complex_abs_eq);
+/*PG_FUNCTION_INFO_V1(complex_abs_eq);*/
 
-Datum
-complex_abs_eq(PG_FUNCTION_ARGS)
-{
-	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+/*Datum*/
+/*complex_abs_eq(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	Complex    *a = (Complex *) PG_GETARG_POINTER(0);*/
+/*	Complex    *b = (Complex *) PG_GETARG_POINTER(1);*/
 
-	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) == 0);
-}
+/*	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) == 0);*/
+/*}*/
 
-PG_FUNCTION_INFO_V1(complex_abs_ge);
+/*PG_FUNCTION_INFO_V1(complex_abs_ge);*/
 
-Datum
-complex_abs_ge(PG_FUNCTION_ARGS)
-{
-	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+/*Datum*/
+/*complex_abs_ge(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	Complex    *a = (Complex *) PG_GETARG_POINTER(0);*/
+/*	Complex    *b = (Complex *) PG_GETARG_POINTER(1);*/
 
-	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) >= 0);
-}
+/*	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) >= 0);*/
+/*}*/
 
-PG_FUNCTION_INFO_V1(complex_abs_gt);
+/*PG_FUNCTION_INFO_V1(complex_abs_gt);*/
 
-Datum
-complex_abs_gt(PG_FUNCTION_ARGS)
-{
-	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+/*Datum*/
+/*complex_abs_gt(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	Complex    *a = (Complex *) PG_GETARG_POINTER(0);*/
+/*	Complex    *b = (Complex *) PG_GETARG_POINTER(1);*/
 
-	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) > 0);
-}
+/*	PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) > 0);*/
+/*}*/
 
-PG_FUNCTION_INFO_V1(complex_abs_cmp);
+/*PG_FUNCTION_INFO_V1(complex_abs_cmp);*/
 
-Datum
-complex_abs_cmp(PG_FUNCTION_ARGS)
-{
-	Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-	Complex    *b = (Complex *) PG_GETARG_POINTER(1);
+/*Datum*/
+/*complex_abs_cmp(PG_FUNCTION_ARGS)*/
+/*{*/
+/*	Complex    *a = (Complex *) PG_GETARG_POINTER(0);*/
+/*	Complex    *b = (Complex *) PG_GETARG_POINTER(1);*/
 
-	PG_RETURN_INT32(complex_abs_cmp_internal(a, b));
-}
+/*	PG_RETURN_INT32(complex_abs_cmp_internal(a, b));*/
+/*}*/
